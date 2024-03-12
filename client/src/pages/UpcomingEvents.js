d8c8915b96af0a75d767b780b51dceff241c6b5e
Karandeep Singh <103121051@nitt.edu>
2024-03-12 15:31:10 +0530

    Data is getting retrieved and shown in the formm of box components

--- client/src/pages/UpcomingEvents.js
+++ client/src/pages/UpcomingEvents.js
@@ -1,8 +1,50 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import './EventBox.css';
 
 
 const UpcomingEvent = () => {
+
+
+  // const [showEmailInput, setShowEmailInput] = useState(false);
+  // const [email, setEmail] = useState('');
+
+  // const handleRegisterClick = () => {
+  //   setShowEmailInput(true);
+  // };
+
+  // const handleEmailChange = (e) => {
+  //   setEmail(e.target.value);
+  // };
+
+  const handleRegister = () => {
+    // Here you can integrate with Gmail or perform any other action with the entered email
+    console.log('Registered with email:');
+    // You can reset the email state if needed
+    // setEmail('');
+    // You can also close the email input field if needed
+    // setShowEmailInput(false);
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -22,21 +64,45 @@ const UpcomingEvent = () => {
 
 
   return (
-    <div className='h-screen-center'>
+    // <div className='h-screen-center'>
         
         <div className="App">
-      <h1>Data from MongoDB</h1>
+      <h1>Upcoming events</h1>
       <div className="data-container">
         {data.map((item, index) => (
-          <div key={index} className="data-box">
-            <h2>{item.eventName}</h2>
-            <p>{item.description}</p>
-            {/* Other data fields */}
+
+
+          <div className="event-box">
+            <h2 className="event-name">{item.eventName}</h2>
+            <div className="event-details">
+              {/* <p><span>Event Type:</span>{item.eventT}</p> */}
+              <p><span>Event Timings: </span>{item.date}, {item.time}</p>
+              <p><span>Venue: </span>{item.venue}</p>
+              {item.ticketType === 'Paid' && (
+                <p><span>Ticket Price: </span> Rs.{item.ticketPrice}</p>
+              )}
+              {item.ticketType === 'Free' && (
+                <p><span>Ticket Price: </span> Free</p>
+              )}
+
+              {/* <p><span>Ticket Price: </span> Rs.{item.ticketPrice}</p> */}
+              <p><span>Event Description: </span>{item.description}</p>
+            </div>
+            {/* <button className="register-button" onClick={handleRegisterClick}>Register</button> */}
+              <div className="email-input">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                />
+                <button className="submit-button" onClick={handleRegister}>Register</button>
+              </div>
+            
           </div>
+
         ))}
       </div>
     </div>
-    </div>
+    // </div>
    
   )
 }
